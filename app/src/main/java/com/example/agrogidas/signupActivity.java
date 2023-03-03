e8196cf1e65219c79c6be36baae906bce7e97fd2
k0lro <gytis1478@gmail.com>
2023-03-03 20:51:47 +0200

    k0lro

--- app/src/main/java/com/example/agrogidas/signupActivity.java
+++ app/src/main/java/com/example/agrogidas/signupActivity.java
@@ -2,13 +2,37 @@ package com.example.agrogidas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class signupActivity extends AppCompatActivity {
 
+    TextView prisi;
+    Button regist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        prisi = findViewById(R.id.prisijung);
+        regist = findViewById(R.id.buttonReg);
+        regist.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(signupActivity.this,loginActivity.class));
+            }
+        });
+        prisi.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(signupActivity.this,loginActivity.class));
+            }
+        });
     }
 }
