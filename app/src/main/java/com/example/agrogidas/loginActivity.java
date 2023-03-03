9bc430d128ee863fd11b37c6021ca68f688c0940
nindrom <nindrom2@gmail.com>
2023-03-03 18:46:41 +0200

    asdasd

--- app/src/main/java/com/example/agrogidas/loginActivity.java
+++ app/src/main/java/com/example/agrogidas/loginActivity.java
@@ -2,13 +2,28 @@ package com.example.agrogidas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginActivity extends AppCompatActivity {
 
+    Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.LoginButton);
+
+        signIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(loginActivity.this,MainActivity.class));
+            }
+        });
     }
 }
