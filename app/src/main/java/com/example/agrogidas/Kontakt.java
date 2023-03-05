08c111320215dd8f708d5ec64f58d2d74f3508d0
Lukas <lukas.peleckis123@gmail.com>
2023-03-05 23:52:11 +0200

    lukutis

--- app/src/main/java/com/example/agrogidas/Kontakt.java
+++ app/src/main/java/com/example/agrogidas/Kontakt.java
@@ -2,23 +2,48 @@ package com.example.agrogidas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+
+
 
 public class Kontakt extends AppCompatActivity {
 
+    private ImageView logo;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kontakt);
+
+
+
+
 
         //custom image for action bar start
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.home_bar_logo, null);
         getSupportActionBar().setCustomView(view);
+
         //custom image for action bar end
+
+        logo = findViewById(R.id.logog);
+        logo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(Kontakt.this,MainActivity.class));
+
+            }
+        });
+
+
     }
 }
