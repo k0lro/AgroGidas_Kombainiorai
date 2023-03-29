f445797dcaad60f9840a6b27b59e712fd9bd3120
gustisdas <nindrom2@gmail.com>
2023-03-29 18:43:22 +0300

    asdfsf

---
+++ app/src/main/java/com/example/agrogidas/short_ad.java
@@ -0,0 +1,14 @@
+package com.example.agrogidas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class short_ad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_short_ad);
+    }
+}
